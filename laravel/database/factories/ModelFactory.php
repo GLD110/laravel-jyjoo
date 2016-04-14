f0e46cc3994df01e03aa1a57d050ff0419c6597c
josef779 <josef779@outlook.com>
2016-04-14 19:42:15 +0300

    Database for CMS
    Migration and Seed are creadted.

--- laravel/database/factories/ModelFactory.php
+++ laravel/database/factories/ModelFactory.php
@@ -1,21 +1,29 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
-
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
         'email' => $faker->safeEmail,
+        'name' => $faker->name,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Category::class, function (Faker\Generator $faker) {
+    return [
+        'color' => $faker->hexColor,
+        'description' => $faker->sentence(5),
+        'language_id' => $faker->numberBetween(1, 2),
+        'title' => $faker->sentence(5)
+    ];
+});
+
+$factory->define(App\Article::class, function (Faker\Generator $faker) {
+    return [
+        'category_id' => $faker->numberBetween(1, 5),
+        'content' => implode("<br/><br/>", $faker->paragraphs(8)),
+        'description' => $faker->sentence(5),
+        'published_at' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'title' => $faker->sentence(5)
     ];
 });
